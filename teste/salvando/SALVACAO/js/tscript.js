fa012c3828aa85e760ae0b1e7a06c80fee7a836a
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2023-11-28 13:45:48 -0300

    modelo

---
+++ teste/salvando/SALVACAO/js/tscript.js
@@ -0,0 +1,20 @@
+let menutoggle = document.querySelector('.toggle');
+menutoggle.onclick = function() {
+	menutoggle.classList.toggle('active')
+}
+const btn_toggle = document.querySelector('.btn-toggle')
+const sidebar = document.querySelector('.sidebar')
+const logo = document.querySelector('.logo')
+const wrapper = document.querySelector('.wrapper')
+
+btn_toggle.addEventListener("click", () => {
+	sidebar.classList.toggle("active");
+	if(sidebar.classList.contains('active')) {
+		logo.setAttribute("style", "display:flex");
+		wrapper.setAttribute("style", "left:240px;width: calc(100% - 240px)");
+		return
+	}
+	logo.setAttribute("style", "display:none");
+	wrapper.setAttribute("style", "left:85px;width: calc(100% - 85px)");
+	
+})
